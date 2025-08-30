649817e5fdefc8a0ddf53a6262c80f84dac263e9
Manjur <manjursk209@gmail.com>
2025-08-30 20:54:16 +0530

    Navbar updated with conditional Upload button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,18 @@
-import { Link } from 'react-router'
+import { Link, useLocation } from 'react-router'
 
 const Navbar = () => {
+  const {pathname} = useLocation();
   return (
     <nav className="navbar">
         <Link to='/'>
             <p className="text-2xl font-bold text-gradient">Resumine</p>
         </Link>
-        <Link to='/upload'>
-            <p className="primary-button w-fit">Upload Resume</p>
-        </Link>
+        {
+          pathname !== '/upload' &&
+            <Link to='/upload'>
+                <p className="primary-button w-fit">Upload Resume</p>
+            </Link>
+        }
     </nav>
   )
 }
